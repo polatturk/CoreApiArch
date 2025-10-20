e800c55877a6cdbafeda46684d4f405bf0e71f3e
polat türk <polatturr52@gmail.com>
2025-10-20 14:48:55 +0300

    Add core database models (entities) and related NuGet packages

---
+++ Core/Entities/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Book : BaseEntity
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; } /* Description olmak zorunda degil o yuzden "?" isareti ile null olabilir diye birakiyorum*/
+        public int CountOfPage { get; set; }
+
+
+        public int AuthorId { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
