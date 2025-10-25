21de44309eb54da0510d4dc88229f1cb0d71d45f
polat türk <polatturr52@gmail.com>
2025-10-25 21:48:58 +0300

    Add User and Book API

---
+++ CoreApiArch/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using Business.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApiArch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        public readonly IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            var books = _bookService.ListAll();
+
+            return Ok(books);
+        }
+    }
+}
---
+++ CoreApiArch/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Business.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApiArch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            var users = _userService.ListAll();
+
+            return Ok(users);
+        }
+    }
+}
