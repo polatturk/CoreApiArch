b295aba2511e1185cf80baed6dbb1e1da1fa5dfb
polat türk <polatturr52@gmail.com>
2025-10-25 12:20:56 +0300

    Add AuthorAPI

---
+++ CoreApiArch/Controllers/AuthorController.cs
@@ -0,0 +1,28 @@
+﻿using Business.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApiArch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+       
+        public readonly IAuthorService _authorService;
+
+        public AuthorController(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            var authors = _authorService.ListAll();
+
+            return Ok(authors);
+        }
+    }
+}
