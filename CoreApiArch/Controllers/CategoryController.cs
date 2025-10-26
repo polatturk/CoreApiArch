f0c50e1144499c5c04ba628c4a514d950bb106ab
polat türk <polatturr52@gmail.com>
2025-10-26 13:43:11 +0300

    Add Category API

---
+++ CoreApiArch/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Business.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApiArch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        public readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            var categories = _categoryService.ListAll();
+
+            return Ok(categories);
+        }
+    }
+}
