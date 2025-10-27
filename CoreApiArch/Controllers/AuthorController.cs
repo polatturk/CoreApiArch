dd40d7b5aa395c7f3c318abb6099c2c6b32c994d
polat türk <polatturr52@gmail.com>
2025-10-27 12:19:33 +0300

    Add Author APIs

--- CoreApiArch/Controllers/AuthorController.cs
+++ CoreApiArch/Controllers/AuthorController.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Interfaces;
+using Core.Entities;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreApiArch.Controllers
@@ -22,7 +24,58 @@ namespace CoreApiArch.Controllers
         {
             var authors = _authorService.ListAll();
 
+            if (authors == null)
+            {
+                return BadRequest("Yazar bulunamadı.");
+            }
+
             return Ok(authors);
+        }
+
+        [HttpGet("GetByName")]
+
+        public IActionResult GetByName(string name)
+        {
+            var result = _authorService.GetByName(name);
+
+            if (result == null)
+            {
+                return BadRequest("Yazar bulunamadı.");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult Delete(int id)
+        {
+
+            var result = _authorService.Delete(id);
+            if(result == null)
+            {
+                return BadRequest("Silme işlemi başarısız oldu.");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost("Create")]
+        public IActionResult Create([FromBody] Author author)
+        {
+            if (author == null)
+            {
+                return BadRequest("Yazar bilgileri boş olamaz.");
+
+            }
+
+            var result = _authorService.Create(author);
+
+            if (result == null)
+            {
+                return BadRequest("Yazar oluşturulamadı.");
+            }
+            return Ok(result);
+
         }
     }
 }
