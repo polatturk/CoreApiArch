68f4227a44529e019bc9ff9791dee554d2a8dc3f
polat türk <polatturr52@gmail.com>
2025-11-08 23:50:40 +0300

    Add CategoryDto

---
+++ DataAccess/DTOs/CategoryDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DTOs
+{
+    public class CategoryDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class CategoryListDto
+    {
+        public int Id { get; set; }
+        public DateTime RecordDate { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
