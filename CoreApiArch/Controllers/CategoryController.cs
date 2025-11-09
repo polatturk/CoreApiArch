f39e4013a9a6b5e702778025f383f55bc14df31b
polat türk <polatturr52@gmail.com>
2025-11-09 17:34:30 +0300

    Add CategoryController

--- CoreApiArch/Controllers/CategoryController.cs
+++ CoreApiArch/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Interfaces;
+using Business.Services;
+using DataAccess.DTOs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,7 +22,56 @@ namespace CoreApiArch.Controllers
         {
             var categories = _categoryService.ListAll();
 
+            if (!categories.IsSuccess)
+            {
+                return NotFound("Kategori bulunamadı.");
+            }
+
             return Ok(categories);
+        }
+
+        [HttpGet("GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            var result = _categoryService.GetByName(name);
+
+            if (!result.IsSuccess)
+            {
+                return NotFound("Kategori bulunamadı.");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult Delete(int id)
+        {
+
+            var result = _categoryService.Delete(id);
+            if (!result.IsSuccess)
+            {
+                return BadRequest("Silme işlemi başarısız oldu.");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost("Create")]
+        public IActionResult Create([FromBody] CategoryDto categoryDto)
+        {
+            if (categoryDto == null)
+            {
+                return BadRequest("Kategori tap bilgileri boş olamaz.");
+            }
+
+            var result = _categoryService.Create(categoryDto);
+
+            if (!result.Result.IsSuccess)
+            {
+                return BadRequest("Kategori oluşturulamadı.");
+            }
+            return Ok(result);
+
         }
     }
 }
