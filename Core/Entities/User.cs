a48db33dacb7e4484af85aabf52d91b163303356
polat türk <polatturr52@gmail.com>
2025-11-26 20:39:40 +0300

    feat(User): Make Username field mandatory

--- Core/Entities/User.cs
+++ Core/Entities/User.cs
@@ -10,7 +10,7 @@ namespace Core.Entities
     {
         public string Name { get; set; }
         public string Surname { get; set; }
-        public string? Username { get; set; }
+        public string Username { get; set; }
         public string? Email { get; set; }
         public string Password { get; set; }
 
